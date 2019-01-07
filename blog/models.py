ad5050a1e4a0cc3c1e62cca1eab76f7aef07700d
Jan Komárek <komret@gmail.com>
2019-01-07 11:45:14 +0100

    Add login/logout and profile

--- blog/models.py
+++ blog/models.py
@@ -7,3 +7,6 @@ class Post(models.Model):
     content = models.TextField()
     date_posted = models.DateTimeField(auto_now_add=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
